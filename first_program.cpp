f0bed81922d53bf4635612ecccabe682ae91091e
pritamkumarpatna59@gmail.com <pritamkumarpatna@gmail.com>
2026-06-05 00:11:27 +0530

    First Program in C++

---
+++ first_program.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    int age;
+    float salary;
+    cout << "Enter your age :";
+    cin >> age;
+    cout << "Enter your Salary :";
+    cin >> salary;
+    salary = salary + salary * 25 /100;
+    cout << endl  << setw(25) << "Age : " << age;
+    cout << endl << endl << endl <<  setw(50) << "Salary :" << salary;
+
+    return 0;
+}
